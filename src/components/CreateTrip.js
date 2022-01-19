b5fa74db5b89841ea2142609546240fd036a3a14
nealgarcia <nathnlgarcia@gmail.com>
2022-01-18 21:28:19 -0600

    capture states of location, startdate, enddate, from form

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,20 +1,44 @@
 import React, { useState } from 'react';
 
 function CreateTrip({closeModal}) {
+    const [location, setLocation] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
+
+    // send Data to API
+    const postData = () => {
+        console.log(location)
+        console.log(startDate)
+        console.log(endDate)
+
+    }
+
     return (
         <div>
             <form className = "createTripForm">
                 <h3>Create a New Trip</h3>
                 <label>Location 
-                    <input type = "text"></input>
+                    <input 
+                        type = "text"
+                        placeholder = "Location"
+                        onChange = {(ev) => setLocation(ev.target.value)}
+                    ></input>
                 </label>
                 <label>Start Date 
-                    <input type = "date"></input>
+                    <input 
+                        type = "date"
+                        placeholder = "Start Date"
+                        onChange = {(ev => setStartDate(ev.target.value))}
+                    ></input>
                 </label>
                 <label>End Date 
-                    <input type = "date"></input>
+                    <input 
+                        type = "date"
+                        placeholder = "End Date"
+                        onChange = {(ev => setEndDate(ev.target.value))}
+                    ></input>
                 </label>
-                <button>Submit</button>
+                <button onClick = {postData}>Submit</button>
                 <button onClick = {closeModal}>Cancel</button>
             </form>
         </div>
