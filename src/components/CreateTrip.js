0da2dbf3ed8396af65634b8ae26b64f2d3fadc09
nealgarcia <nathnlgarcia@gmail.com>
2022-01-19 13:02:38 -0600

    POST request to API functioning

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,48 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-function CreateTrip({closeModal}) {
-    const [location, setLocation] = useState('')
-    const [startDate, setStartDate] = useState('')
-    const [endDate, setEndDate] = useState('')
+function CreateTrip({ closeModal }) {
+  const [formState, setFormState] = useState({
+    location: "",
+    start_date: "",
+    end_date: "",
+  })
 
-    // send Data to API
-    const postData = () => {
-        console.log(location)
-        console.log(startDate)
-        console.log(endDate)
+  // const [location, setLocation] = useState("");
+  // const [startDate, setStartDate] = useState("");
+  // const [endDate, setEndDate] = useState("");
 
-    }
+  const handleChange = (ev) => {
+    setFormState({...formState, [ev.target.name]: ev.target.value})
+    console.log(formState)
+  }
 
-    return (
-        <div>
-            <form className = "createTripForm">
-                <h3>Create a New Trip</h3>
-                <label>Location 
-                    <input 
-                        type = "text"
-                        placeholder = "Location"
-                        onChange = {(ev) => setLocation(ev.target.value)}
-                    ></input>
-                </label>
-                <label>Start Date 
-                    <input 
-                        type = "date"
-                        placeholder = "Start Date"
-                        onChange = {(ev => setStartDate(ev.target.value))}
-                    ></input>
-                </label>
-                <label>End Date 
-                    <input 
-                        type = "date"
-                        placeholder = "End Date"
-                        onChange = {(ev => setEndDate(ev.target.value))}
-                    ></input>
-                </label>
-                <button onClick = {postData}>Submit</button>
-                <button onClick = {closeModal}>Cancel</button>
-            </form>
-        </div>
-    );
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    console.log(formState);
+    axios({
+      method: 'post',
+      url: `http://localhost:8000/trips/`,
+      data: formState
+    })
+  }
+
+
+  return (
+    <div>
+      <form className="createTripForm">
+        <h3>Create a New Trip</h3>
+        <label>
+          Location
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            // onChange={(ev) => setLocation(ev.target.value)}
+            onChange = {handleChange}
+          ></input>
+        </label>
+        <label>
+          Start Date
+          <input
+            type="date"
+            name="start_date"
+            placeholder="Start Date"
+            // onChange={(ev) => setStartDate(ev.target.value)}
+            onChange = {handleChange}
+          ></input>
+        </label>
+        <label>
+          End Date
+          <input
+            type="date"
+            name="end_date"
+            placeholder="End Date"
+            // onChange={(ev) => setEndDate(ev.target.value)}
+            onChange = {handleChange}
+          ></input>
+        </label>
+        <button onClick = {handleSubmit}>Submit</button>
+        <button onClick={closeModal}>Cancel</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateTrip;
