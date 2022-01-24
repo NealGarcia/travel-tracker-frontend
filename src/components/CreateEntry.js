00531f929a98816c4a3e7523e69c56aeb7b89b92
nealgarcia <nathnlgarcia@gmail.com>
2022-01-23 23:25:01 -0600

    modal styling

--- src/components/CreateEntry.js
+++ src/components/CreateEntry.js
@@ -6,10 +6,10 @@ function CreateEntry({ closeModal, entry, data }) {
     photo_url: "",
     body: "",
     date: "",
-    trip_id: data.id
+    trip_id: data.id,
   });
 
-  console.log(data)
+  console.log(data);
 
   // Event handlers to capture data from form
   const handleChange = (ev) => {
@@ -30,34 +30,43 @@ function CreateEntry({ closeModal, entry, data }) {
     <div>
       <form className="createEntryForm">
         <h3>New Entry</h3>
-        <label>
-          Image
-          <input
-            type="text"
-            name="photo_url"
-            placeholder="Image URL"
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label>
-          Date
-          <input
-            type="date"
-            name="date"
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label>
-          Description
-          <textarea
-            type="text"
-            name="body"
-            placeholder="Description"
-            onChange={handleChange}
-          ></textarea>
-        </label>
-        <button onClick={handleSubmit}>Submit</button>
-        <button onClick={closeModal}>Cancel</button>
+        <ul className="formWrapper">
+        <li className="formRow">
+          <label>
+            Image
+            <input
+              type="text"
+              name="photo_url"
+              placeholder="Image URL"
+              onChange={handleChange}
+            ></input>
+          </label>
+          </li>
+          <li className="formRow">
+          <label>
+            Date
+            <input type="date" name="date" onChange={handleChange}></input>
+          </label>
+          </li>
+          <li className="formRow">
+          <label>
+            Description
+            <textarea
+              type="text"
+              name="body"
+              placeholder="Description"
+              onChange={handleChange}
+              id = "description"
+            ></textarea>
+          </label>
+          </li>
+          <li className="formRow">
+          <button onClick={handleSubmit}id = "submit">Submit</button>
+          </li>
+          <li className="formRow">
+          <button onClick={closeModal}id = "cancel">Cancel</button>
+          </li>
+        </ul>
       </form>
     </div>
   );
--- src/components/EditTrip.js
+++ src/components/EditTrip.js
@@ -1,63 +1,82 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { Redirect, Link } from "react-router-dom";
 
-function EditTrip({entry, data, closeTripModal}) {
-    const [formState, setFormState] = useState({
-        location: "",
-        start_date: "",
-        end_date: "",
-        entry: [],
-      });
+function EditTrip({ entry, data, closeTripModal }) {
+  const [formState, setFormState] = useState({
+    location: "",
+    start_date: "",
+    end_date: "",
+    entry: [],
+  });
 
-      const handleChange = (ev) => {
-        setFormState({ ...formState, [ev.target.name]: ev.target.value });
-        console.log(formState);
-      };
+  const handleChange = (ev) => {
+    setFormState({ ...formState, [ev.target.name]: ev.target.value });
+    console.log(formState);
+  };
 
-      const handleSubmit = (ev) => {
-        console.log(formState);
-        axios({
-          method: "patch",
-          url: `${process.env.REACT_APP_API_URL}/trips/${data.id}`,
-          data: formState,
-        });
-      };
+  const handleSubmit = (ev) => {
+    console.log(formState);
+    axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}/trips/${data.id}`,
+      data: formState,
+    });
+  };
 
-      function onDelete(){
-          axios.delete(`${process.env.REACT_APP_API_URL}/trips/${data.id}`)
-      }
-      
-      console.log(data.id)
+  function onDelete() {
+    axios.delete(`${process.env.REACT_APP_API_URL}/trips/${data.id}`);
+  }
+
+  console.log(data.id);
 
   return (
     <div>
       <div>
         <form className="editTripForm">
-          <h3>Edit Trip</h3>
-          <label>
-            Location
-            <input
-              type="text"
-              name="location"
-              placeholder="Location"
-              onChange={handleChange}
-            ></input>
-          </label>
-          <label>
-            Start Date
-            <input
-              type="date"
-              name="start_date"
-              onChange={handleChange}
-            ></input>
-          </label>
-          <label>
-            End Date
-            <input type="date" name="end_date" onChange={handleChange}></input>
-          </label>
-          <button onClick={handleSubmit}>Submit</button>
-          <button onClick = {onDelete}>Delete</button>
-          <button onClick={closeTripModal}>Cancel</button>
+          <ul className="formWrapper">
+            <h3>Edit Trip</h3>
+            <li className="formRow">
+              <label>
+                Location
+                <input
+                  type="text"
+                  name="location"
+                  placeholder="Location"
+                  onChange={handleChange}
+                ></input>
+              </label>
+            </li>
+            <li className="formRow">
+              <label>
+                Start Date
+                <input
+                  type="date"
+                  name="start_date"
+                  onChange={handleChange}
+                ></input>
+              </label>
+            </li>
+            <li className="formRow">
+              <label>
+                End Date
+                <input
+                  type="date"
+                  name="end_date"
+                  onChange={handleChange}
+                ></input>
+              </label>
+            </li>
+            <li className="formRow">
+            <button onClick={handleSubmit} id = "submit">Submit</button>
+            </li>
+            <li className="formRow">
+            <Link to ='/' onClick={onDelete}><button id = "cancel">Delete</button></Link>
+            </li>
+            <li className="formRow">
+            <button onClick={closeTripModal} id = "cancel">Cancel</button>
+            </li>
+          </ul>
         </form>
       </div>
     </div>
