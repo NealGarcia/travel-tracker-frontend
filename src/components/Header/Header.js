1dfbadac5b2bd1e8e8b38af6e8c08808c2221a95
nealgarcia <nathnlgarcia@gmail.com>
2022-03-16 22:06:09 -0500

    update header so that new trip modal is opened when 'add trip' is clicked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
+import Modal from "react-modal";
+import CreateTrip from "../CreateTrip";
+
+
+// modal styles
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "10px",
+    border: "0.5px solid white",
+    backgroundColor: "rgb(200, 200, 200)",
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+};
 
 function Header(props) {
-    return (
-        <div className = 'header'>
-            <div className = "navBar">
-                <h1 className = "title"><Link to="/">travlr</Link></h1>
-                <div className = "navLinks">
-                    <a href="#Trips" className = "nav">View Trips</a>
-                    <a href="#add" className = "nav">Add Trip</a>
-                    <a href="#search" className = "nav">About</a>
-                </div>
-            </div>
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  // Modal open/close event handlers
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div className="header">
+      <div className="navBar">
+        <h1 className="title">
+          <Link to="/">travlr</Link>
+        </h1>
+        <div className="navLinks">
+          <a href="#Trips" className="nav">
+            View Trips
+          </a>
+          <a href="#add" className="nav" onClick={openModal}>
+            Add Trip
+          </a>
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Create New Trip"
+          >
+            <CreateTrip closeModal={closeModal} />
+          </Modal>
+          <a href="#search" className="nav">
+            About
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
